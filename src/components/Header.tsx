74c40405489a015cc675d1fae033a7336c794776
ChristieCardoso <christiecardoso@hotmail.com>
2024-01-29 17:30:13 -0300

    edit header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
 import { ConnectWallet } from "@thirdweb-dev/react";
-
 import styles from "../styles/Header.module.css";
 
-export const Header = () => {
+export const Header = ({ showConnectWalletLink }) => {
   return (
     <nav className={styles.header}>
-      <ConnectWallet theme="dark" />
+      <Link href="/">
+        <img
+          src="/thirdweb.svg"
+          alt="thirdweb"
+          width={52}
+          height={32}
+          className={styles.logo}
+        />
+      </Link>
+      <Link href="/">Home</Link>
+      <Link href="login">Login</Link>
+      <Link href="afiliados">Afiliados</Link>
+      <Link href="buyNft">Comprar NFT</Link>
+      {showConnectWalletLink && <ConnectWallet theme="dark" />}
     </nav>
   );
 };
